983e288be1aece145e91331d6714dfdc40b94f72
ushkdn <ushkandn@gmail.com>
2024-05-31 09:33:58 +0300

    initial commit

---
+++ src/main/java/headhunter_webapi/HeadhunterWebapiApplication.java
@@ -0,0 +1,13 @@
+package headhunter_webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadhunterWebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadhunterWebapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/headhunter_webapi/HeadhunterWebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package headhunter_webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadhunterWebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
