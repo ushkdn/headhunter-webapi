5b9d06048a499d24fda36365c6743835a10af8f2
ushkdn <ushkndn@gmail.com>
2024-06-10 21:19:12 +0300

    added email interface

--- src/main/java/headhunter_webapi/service/emailService/EmailService.java
+++ src/main/java/headhunter_webapi/service/emailService/EmailService.java
@@ -1,7 +1,25 @@
 package headhunter_webapi.service.emailService;
 
+import headhunter_webapi.entity.ServiceResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService implements IEmailService{
+    @Override
+    public ServiceResponse<String> sendEmail(String topic, String recipientEmail) {
+    }
+
+    @Override
+    public ServiceResponse<String> verifyEmail(Long id, String securityCode) {
+        return null;
+    }
+
+    @Override
+    public ServiceResponse<String> resendCode(Long id) {
+        return null;
+    }
+
+    private String CreateSecurityCode(){
+
+    }
 }
--- src/main/java/headhunter_webapi/service/emailService/IEmailService.java
+++ src/main/java/headhunter_webapi/service/emailService/IEmailService.java
@@ -1,5 +1,9 @@
 package headhunter_webapi.service.emailService;
 
+import headhunter_webapi.entity.ServiceResponse;
+
 public interface IEmailService {
-    
+    ServiceResponse<String> sendEmail(String topic, String recipientEmail);
+    ServiceResponse<String> verifyEmail(Long id, String securityCode);
+    ServiceResponse<String> resendCode(Long id);
 }
